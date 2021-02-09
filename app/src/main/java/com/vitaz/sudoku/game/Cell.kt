d9b1d60802d28a0d890708c083a7c18d8fa62b3b
vit_az <vit.azarov@gmail.com>
2021-02-09 06:14:15 -0400

    added new cell parameter - isStartingCell. new logic to proceed and interact with cell if it is a starting one

--- app/src/main/java/com/vitaz/sudoku/game/Cell.kt
+++ app/src/main/java/com/vitaz/sudoku/game/Cell.kt
@@ -3,7 +3,8 @@ package com.vitaz.sudoku.game
 class Cell(
     val row: Int,
     val col: Int,
-    var value: Int
+    var value: Int,
+    var isStartingCell: Boolean = false
 ) {
 
 }
--- app/src/main/java/com/vitaz/sudoku/game/SudokuGame.kt
+++ app/src/main/java/com/vitaz/sudoku/game/SudokuGame.kt
@@ -14,6 +14,8 @@ class SudokuGame {
 
     init {
         val cells = List(9 * 9) {i -> Cell(i / 9, i % 9, i % 9)}
+        cells[11].isStartingCell = true
+        cells[21].isStartingCell = true
         board = Board(9, cells)
 
         selectedCellLiveData.postValue(Pair(selectedRow, selectedColumn))
@@ -22,15 +24,18 @@ class SudokuGame {
 
     fun handleInput(number: Int) {
         if (selectedRow == -1 || selectedColumn == -1) return
+        if (board.getCell(selectedRow, selectedColumn).isStartingCell) return
 
         board.getCell(selectedRow, selectedColumn).value = number
         cellsLiveData.postValue(board.cells)
     }
 
     fun updateSelectedCell (row: Int, col: Int) {
-        selectedRow = row
-        selectedColumn = col
-        selectedCellLiveData.postValue(Pair(row, col))
+        if (!board.getCell(row, col).isStartingCell) {
+            selectedRow = row
+            selectedColumn = col
+            selectedCellLiveData.postValue(Pair(row, col))
+        }
     }
 
 }
